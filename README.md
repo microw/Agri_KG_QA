53974329ae991d3b6afa9385a811b77756b220db
yimoru <447768433@qq.com>
2023-03-20 21:29:52 +0800

    add run file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-
+## 项目目录
 D:.
 ├─.idea
 │  └─inspectionProfiles
@@ -8,3 +8,10 @@ D:.
 ├─data_process      保存有原始数据，并将所需数据抽取出来
 ├─dict              保存抽取出的实体部分
 └─templates
+
+## 数据处理
+1. 将以下数据下载后放在data_process中
+数据提取：
+链接：https://pan.baidu.com/s/1ZJ9vwr2MdWhuYsM8_-AOaA?pwd=3lmn 
+提取码：3lmn 
+2. 运行data_process文件夹下的main.py文件夹。(运行前请连接neo4j)
--- data_process/main.py
+++ data_process/main.py
@@ -5,12 +5,21 @@
 # @FileName: main.py
 # @Software: PyCharm
 
-# import build_graph
+import build_graph
 import data_process.data_city2weather as cw
 import data_process.data_plant2nutrition as pn
+import shutil
+import os
 
 if __name__ == '__main__':
+
     cw.main()
     pn.main()
+
+    father_path = os.path.abspath('..')
+    shutil.copy('csv\weather_plant.csv', os.path.join(father_path, 'data'))
+    os.renames(os.path.join(father_path, 'data\weather_plant.csv'),
+               os.path.join(father_path, 'data\weather2plant.csv'))
+
     build_graph.main()
 
